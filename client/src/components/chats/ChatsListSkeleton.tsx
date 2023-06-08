1d6909eade23271b2a5e9b9a75f86bd596748af0
Lior Ashkenazi <lior.ashkenazi>
2023-06-08 17:38:37 +0300

    Changed the chats list skeleton

--- client/src/components/chats/ChatsListSkeleton.tsx
+++ client/src/components/chats/ChatsListSkeleton.tsx
@@ -2,7 +2,7 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const ChatsListSkeleton = () => {
-  const skeletonCount = 10;
+  const skeletonCount = 13;
 
   const skeletons = Array.from({ length: skeletonCount }, (_, index) => (
     <Skeleton key={index} height={30} className="mb-2" />
