71dcd1b34c505c1fece41776d39f6b08ffa68cb5
Lior Ashkenazi <lior.ashkenazi>
2023-06-12 16:31:21 +0300

    Added shadow to page header

--- client/src/components/chats/PageHeader.tsx
+++ client/src/components/chats/PageHeader.tsx
@@ -1,7 +1,7 @@
 const PageHeader = () => {
   return (
-    <div className="bg-indigo-800 px-3 py-2 flex justify-between">
-      <h1 className="font-mono text-2xl text-gray-50 font-semibold tracking-tighter drop-shadow-md outlined-text">
+    <div className="bg-indigo-800 px-3 py-2 flex justify-between drop-shadow-md">
+      <h1 className="font-mono text-2xl text-gray-50 font-semibold tracking-tighter drop-shadow-sm outlined-text">
         ProgCom
       </h1>
       <div>hello</div>
