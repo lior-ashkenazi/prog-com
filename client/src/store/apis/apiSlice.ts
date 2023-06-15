3472c81a2d461c6879c2ccf394f967f60daf7646
Lior Ashkenazi <lior.ashkenazi>
2023-06-15 12:01:30 +0300

    Made changes in network with server in client, including adding new environment variable

--- client/src/store/apis/apiSlice.ts
+++ client/src/store/apis/apiSlice.ts
@@ -1,13 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../index";
 
+const NODE_ENV = import.meta.env.VITE_NODE_ENV as string;
 const ENDPOINT = import.meta.env.VITE_ENDPOINT as string;
 
 export const apiSlice = createApi({
-  reducerPath: `${ENDPOINT}/api`,
+  reducerPath: "api",
   tagTypes: ["Users", "Messages", "Chats", "Profile"],
   baseQuery: fetchBaseQuery({
-    baseUrl: "/api",
+    baseUrl: `${NODE_ENV === "production" ? ENDPOINT : ""}/api`,
     prepareHeaders: (headers, { getState }) => {
       const token: string | null = (getState() as RootState).auth.token;
       if (token) {
