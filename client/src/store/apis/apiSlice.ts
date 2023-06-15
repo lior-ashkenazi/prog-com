0257cc36323138afef5c582f10c9bb3180fdb7db
Lior Ashkenazi <lior.ashkenazi>
2023-06-15 11:41:34 +0300

    Changed reducerPath back in apiSlice

--- client/src/store/apis/apiSlice.ts
+++ client/src/store/apis/apiSlice.ts
@@ -1,8 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../index";
 
+const ENDPOINT = import.meta.env.VITE_ENDPOINT as string;
+
 export const apiSlice = createApi({
-  reducerPath: "api",
+  reducerPath: `${ENDPOINT}/api`,
   tagTypes: ["Users", "Messages", "Chats", "Profile"],
   baseQuery: fetchBaseQuery({
     baseUrl: "/api",
